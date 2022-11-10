d9904adbdbd3f7857e605ef8419b09fa7eefdc9a
minhhoangdev1 <minhhoang.blacksoft@gmail.com>
2022-11-10 17:12:27 +0700

    done eav

---
+++ Movie/Setup/Patch/Data/AddAttributeCustomer.php
@@ -0,0 +1,145 @@
+<?php declare(strict_types=1);
+
+namespace Magenest\Movie\Setup\Patch\Data;
+
+use Exception;
+use Psr\Log\LoggerInterface;
+use Magento\Customer\Api\CustomerMetadataInterface;
+use Magento\Customer\Model\ResourceModel\Attribute as AttributeResource;
+use Magento\Customer\Setup\CustomerSetup;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+/**
+ * Creates a customer attribute for managing a customer's external system ID
+ */
+class AddAttributeCustomer implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var CustomerSetup
+     */
+    private $customerSetup;
+
+    /**
+     * @var AttributeResource
+     */
+    private $attributeResource;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CustomerSetupFactory $customerSetupFactory
+     * @param AttributeResource $attributeResource
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeResource $attributeResource,
+        LoggerInterface $logger
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->customerSetup = $customerSetupFactory->create(['setup' => $moduleDataSetup]);
+        $this->attributeResource = $attributeResource;
+        $this->logger = $logger;
+    }
+
+    /**
+     * Get array of patches that have to be executed prior to this.
+     *
+     * Example of implementation:
+     *
+     * [
+     *      \Vendor_Name\Module_Name\Setup\Patch\Patch1::class,
+     *      \Vendor_Name\Module_Name\Setup\Patch\Patch2::class
+     * ]
+     *
+     * @return string[]
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * Get aliases (previous names) for the patch.
+     *
+     * @return string[]
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    /**
+     * Run code inside patch
+     */
+    public function apply()
+    {
+        // Start setup
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        try {
+            // Add customer attribute with settings
+            $this->customerSetup->addAttribute(
+                CustomerMetadataInterface::ENTITY_TYPE_CUSTOMER,
+                'profile_picture',
+                [
+                    'type' => 'varchar',
+                    'label' => 'Profile Picture',
+                    'input' => 'image',
+                    'backend' => '',
+                    'required' => false,
+                    'visible' => true,
+                    'user_defined' => true,
+                    'sort_order' => 95,
+                    'position' => 95,
+                    'system' => 0,
+                    'is_used_in_grid' => true,
+                    'is_visible_in_grid' => true,
+                    'is_html_allowed_on_front' => true,
+                    'visible_on_front' => true,
+                    'is_filterable_in_grid' => true,
+                    'is_searchable_in_grid' => true,
+                ]
+            );
+
+            // Add attribute to default attribute set and group
+            $this->customerSetup->addAttributeToSet(
+                CustomerMetadataInterface::ENTITY_TYPE_CUSTOMER,
+                CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
+                null,
+                'profile_picture'
+            );
+
+            // Get the newly created attribute's model
+            $attribute = $this->customerSetup->getEavConfig()
+                ->getAttribute(CustomerMetadataInterface::ENTITY_TYPE_CUSTOMER, 'profile_picture');
+
+            // Make attribute visible in Admin customer form
+            $attribute->setData('used_in_forms', [
+                'adminhtml_customer','customer_account_edit','customer_account'
+            ]);
+
+            // Save attribute using its resource model
+            $this->attributeResource->save($attribute);
+        } catch (Exception $e) {
+            $this->logger->err($e->getMessage());
+        }
+
+        // End setup
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+}
